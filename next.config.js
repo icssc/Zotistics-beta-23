2486c79110c534b4e5cdc7fcad8ff2ff1814dca8
Alexander Liu <hey@alexanderliu.com>
2022-01-22 04:13:45 -0800

    build: install cypress, jest, unplugin-icons & tailwindcss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const { FileSystemIconLoader } = require("unplugin-icons/loaders");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  webpack: (config) => {
+    config.plugins.push(
+      require("unplugin-icons/webpack")({
+        compiler: "jsx",
+        jsx: "react",
+        scale: 1,
+        customCollections: {
+          custom: FileSystemIconLoader("./src/assets/icons", (svg) =>
+            svg.replace(/^<svg /, '<svg fill="currentColor" ')
+          ),
+        },
+      })
+    );
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,md,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
