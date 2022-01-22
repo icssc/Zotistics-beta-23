f0bd3ddd632d5190f479b8fb14b735b9045e1a04
Alexander Liu <hey@alexanderliu.com>
2022-01-22 07:09:41 -0800

    fix: update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,32 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ColorSchemeContextProvider initialValue="dark">
       <Head>
         <title>Zotistics</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=2"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=2" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=2"
+          color="#0064a4"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <meta name="msapplication-TileColor" content="#0064a4" />
+        <meta name="theme-color" content="#ffffff" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
           rel="stylesheet"
