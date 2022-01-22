1ad7178acf3017f12a56d078aeca7f2bd232faf7
Alexander Liu <hey@alexanderliu.com>
2022-01-22 04:46:04 -0800

    feat: nav component

---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+import Logo from "~icons/custom/logo.jsx";
+import ReturnUpBack from "~icons/ion/return-up-back-outline.jsx";
+
+import ColorSchemeToggle from "src/components/colorSchemeToggle/colorSchemeToggle";
+
+const Nav = () => {
+  return (
+    <nav className="fixed w-full h-16 text-sm font-light dark:border-b bg-white/75 drop-shadow-lg text-neutral-500 dark:border-neutral-800 dark:bg-black/75 dark:text-neutral-200">
+      <div className="grid grid-cols-3 justify-between items-center m-auto w-full max-w-6xl h-full">
+        <Link href="https://old.zotistics.com">
+          <a className="grid grid-flow-col gap-2 justify-self-start items-center">
+            <div className="text-[24px]">
+              <ReturnUpBack />
+            </div>
+            old.zotistics.com
+          </a>
+        </Link>
+
+        <Link href="/">
+          <a className="text-[32px] justify-self-center">
+            <Logo />
+          </a>
+        </Link>
+
+        <div className="grid grid-flow-col gap-4 justify-self-end items-center">
+          <Link href="/post/faq">
+            <a>FAQ</a>
+          </Link>
+          <Link href="/post/guides">
+            <a>Guides</a>
+          </Link>
+          <Link href="/feedback">
+            <a>Feedback</a>
+          </Link>
+          <div className="ml-2">
+            <ColorSchemeToggle />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
