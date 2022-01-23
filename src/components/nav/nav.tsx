9df1cd4cea080611a7a4dd799c4efb1d9915e382
Alexander Liu <hey@alexanderliu.com>
2022-01-22 18:48:15 -0800

    refactor: create content tailwindcss component

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -7,39 +7,39 @@ import ColorSchemeToggle from "src/components/colorSchemeToggle/colorSchemeToggl
 
 const Nav = () => {
   return (
-    <nav className="fixed w-full h-16 text-sm font-light dark:border-b bg-white/75 drop-shadow-lg text-neutral-500 dark:border-neutral-800 dark:bg-black/75 dark:text-neutral-200">
-      <div className="grid grid-cols-3 justify-between items-center m-auto w-full max-w-6xl h-full">
-        <Link href="https://old.zotistics.com">
-          <a className="grid grid-flow-col gap-2 justify-self-start items-center">
-            <div className="text-[24px]">
-              <ReturnUpBack />
+    <>
+      <nav className="fixed w-full h-16 text-sm font-light dark:border-b bg-white/75 drop-shadow-lg text-neutral-500 dark:border-neutral-800 dark:bg-black/75 dark:text-neutral-200">
+        <div className="grid grid-cols-3 justify-between items-center h-full content">
+          <Link href="https://old.zotistics.com">
+            <a className="grid grid-flow-col gap-2 justify-self-start items-center">
+              <div className="text-[24px]">
+                <ReturnUpBack />
+              </div>
+              old.zotistics.com
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className="text-[32px] justify-self-center">
+              <Logo />
+            </a>
+          </Link>
+
+          <div className="grid grid-flow-col gap-4 justify-self-end items-center">
+            <Link href="/posts/faq">
+              <a>FAQ</a>
+            </Link>
+            <Link href="/feedback">
+              <a>Feedback</a>
+            </Link>
+            <div className="ml-2">
+              <ColorSchemeToggle />
             </div>
-            old.zotistics.com
-          </a>
-        </Link>
-
-        <Link href="/">
-          <a className="text-[32px] justify-self-center">
-            <Logo />
-          </a>
-        </Link>
-
-        <div className="grid grid-flow-col gap-4 justify-self-end items-center">
-          <Link href="/post/faq">
-            <a>FAQ</a>
-          </Link>
-          <Link href="/post/guides">
-            <a>Guides</a>
-          </Link>
-          <Link href="/feedback">
-            <a>Feedback</a>
-          </Link>
-          <div className="ml-2">
-            <ColorSchemeToggle />
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <div className="h-16" />
+    </>
   );
 };
 
