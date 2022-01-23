e76ac7a2b81c2fdec35ee6319bb228f1c6d2a30b
Alexander Liu <hey@alexanderliu.com>
2022-01-23 07:08:01 -0800

    fix: decrease nav opacity

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -8,7 +8,7 @@ import ColorSchemeToggle from "src/components/ColorSchemeToggle/ColorSchemeToggl
 const Nav = () => {
   return (
     <>
-      <nav className="fixed w-full h-16 text-sm font-light dark:border-b bg-white/75 drop-shadow-lg text-neutral-500 dark:border-neutral-800 dark:bg-black/75 dark:text-neutral-200">
+      <nav className="fixed w-full h-16 text-sm font-light dark:border-b bg-white/90 drop-shadow-lg text-neutral-500 dark:border-neutral-800 dark:bg-black/90 dark:text-neutral-200">
         <div className="grid grid-cols-3 justify-between items-center h-full content">
           <Link href="https://old.zotistics.com">
             <a className="grid grid-flow-col gap-2 justify-self-start items-center">
