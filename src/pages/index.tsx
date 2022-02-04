218f42fe3e200538ede042e75ef18020fe3d79f0
Alexander Liu <hey@alexanderliu.com>
2022-02-04 12:43:13 -0800

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
 import type { NextPage } from "next";
+import QueriesProvider from "src/contexts/queries/QueriesProvider";
 import Nav from "src/components/Nav/Nav";
 import Form from "src/components/Form/Form";
+import QuerySelectorList from "src/components/QuerySelectorList/QuerySelectorList";
+import Graph from "src/components/Graph/Graph";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <QueriesProvider
+      initialQueries={
+        new Map([
+          [
+            new Date(),
+            {
+              instructors: [],
+              quarters: [],
+              years: [],
+              departments: [],
+              courseCode: [],
+              classCode: [],
+            },
+          ],
+        ])
+      }
+    >
       <Nav />
-      <Form />
-    </>
+      <main className="mt-10 content">
+        <Form />
+        <QuerySelectorList />
+        <div className="mt-8 h-96">
+          <Graph />
+        </div>
+      </main>
+    </QueriesProvider>
   );
 };
 
