f3ef948e54741f92adf827f0ce8fa0df147afea7
Alexander Liu <hey@alexanderliu.com>
2022-02-04 12:42:50 -0800

    feat: graph component

---
+++ src/components/Graph/Graph.tsx
@@ -0,0 +1,184 @@
+import dynamic from "next/dynamic";
+import { useContext, useEffect, useState } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloQueryResult,
+} from "@apollo/client";
+import { ResponsiveBar } from "@nivo/bar";
+
+import { ColorSchemeContext } from "src/contexts/colorSchemeToggle/colorSchemeContext";
+import { QueriesContext } from "src/contexts/queries/queries";
+
+import { Query } from "src/types/peterportal";
+
+const client = new ApolloClient({
+  uri: "https://api.peterportal.org/graphql/",
+  cache: new InMemoryCache(),
+});
+
+// https://github.com/plouc/nivo/pull/1886
+// @ts-ignore
+const DynamicComponent: typeof ResponsiveBar = dynamic(
+  () => import("@nivo/bar").then((mod) => mod.ResponsiveBar),
+  { ssr: false }
+);
+
+const GET_GRADES = gql`
+  query GetGrades(
+    $year: String!
+    $quarter: String!
+    $instructor: String!
+    $department: String!
+    $number: String!
+    $code: String!
+  ) {
+    grades(
+      year: $year
+      quarter: $quarter
+      instructor: $instructor
+      department: $department
+      number: $number
+      code: $code
+    ) {
+      aggregate {
+        sum_grade_a_count
+        sum_grade_b_count
+        sum_grade_c_count
+        sum_grade_d_count
+        sum_grade_f_count
+        sum_grade_p_count
+        sum_grade_np_count
+        average_gpa
+        count
+      }
+    }
+  }
+`;
+
+const optionsDark = {
+  theme: {
+    textColor: "#ffffff",
+    tooltip: {
+      container: {
+        background: "#000000",
+        color: "#ffffff",
+        fontSize: 12,
+      },
+    },
+  },
+};
+const optionsLight = {};
+
+interface gradeValue {
+  id: "A" | "B" | "C" | "D" | "F" | "P" | "NP";
+  [key: string]: number | string;
+}
+
+const Graph = () => {
+  const { queries, updateQueryState } = useContext(QueriesContext);
+
+  const [queryIdsValue, setQueryIdsValue] = useState<string[]>([]);
+  const [gradeValues, setGradeValues] = useState<gradeValue[]>([]);
+
+  const { colorScheme } = useContext(ColorSchemeContext);
+
+  const props = colorScheme === "dark" ? optionsDark : optionsLight;
+
+  useEffect(() => {
+    const queryIds: string[] = [];
+    queries.forEach((_value, key) => {
+      queryIds.push(key);
+    });
+    setQueryIdsValue(queryIds);
+  }, [queries]);
+
+  useEffect(() => {
+    const graphQueries: Promise<ApolloQueryResult<Query>>[] = [];
+    queries.forEach(async (query, key) => {
+      updateQueryState(key, "loading", true);
+      graphQueries.push(
+        client
+          .query({
+            query: GET_GRADES,
+            variables: {
+              year: query.years.map(({ value }) => value).join(";"),
+              quarter: query.quarters.map(({ value }) => value).join(";"),
+              instructor: query.instructors.map(({ value }) => value).join(";"),
+              department: query.departments.map(({ value }) => value).join(";"),
+              number: query.courseCode.map(({ value }) => value).join(";"),
+              code: query.classCode.map(({ value }) => value).join(";"),
+            },
+          })
+          .then((query) => {
+            updateQueryState(key, "loading", false);
+            return query;
+          })
+      );
+    });
+    Promise.all(graphQueries).then((values) => {
+      const newGradeValues: gradeValue[] = [
+        { id: "A" },
+        { id: "B" },
+        { id: "C" },
+        { id: "D" },
+        { id: "F" },
+        { id: "P" },
+        { id: "NP" },
+      ];
+      values.map(({ data }, i) => {
+        const gradeValue: number[] = [];
+        const gradesAggregate = data.grades.aggregate;
+        const total =
+          gradesAggregate.sum_grade_a_count +
+          gradesAggregate.sum_grade_b_count +
+          gradesAggregate.sum_grade_c_count +
+          gradesAggregate.sum_grade_d_count +
+          gradesAggregate.sum_grade_f_count +
+          gradesAggregate.sum_grade_p_count +
+          gradesAggregate.sum_grade_np_count;
+        gradeValue.push(data.grades.aggregate.sum_grade_a_count / total);
+        gradeValue.push(data.grades.aggregate.sum_grade_b_count / total);
+        gradeValue.push(data.grades.aggregate.sum_grade_c_count / total);
+        gradeValue.push(data.grades.aggregate.sum_grade_d_count / total);
+        gradeValue.push(data.grades.aggregate.sum_grade_f_count / total);
+        gradeValue.push(data.grades.aggregate.sum_grade_p_count / total);
+        gradeValue.push(data.grades.aggregate.sum_grade_np_count / total);
+        // This is probably a little hacky? but it works for now.
+        // Basically it relies on the fact that the order of the results are the same as the ids, which should always be the case because they're both derived from the query context.
+        newGradeValues.forEach((newGradeValue, j) => {
+          newGradeValue[queryIdsValue[i]] = gradeValue[j];
+        });
+      });
+      setGradeValues(newGradeValues);
+    });
+  }, [queryIdsValue, queries]);
+
+  return (
+    <DynamicComponent
+      data={gradeValues}
+      keys={queryIdsValue}
+      groupMode="grouped"
+      axisLeft={{
+        legend: "% of students",
+        legendPosition: "middle",
+        legendOffset: -40,
+      }}
+      axisBottom={{
+        legend: "grade",
+        legendPosition: "middle",
+        legendOffset: 32,
+      }}
+      valueFormat=">-.2%"
+      colors={{ scheme: "set2" }}
+      margin={{ bottom: 50, left: 50 }}
+      padding={0.25}
+      innerPadding={6}
+      borderRadius={4}
+      {...props}
+    />
+  );
+};
+
+export default Graph;
