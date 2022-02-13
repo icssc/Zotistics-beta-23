7f9040ca88bc047afc237d7267e594c2b805c122
Patrick Nguyen <41417417+patrickanguyen@users.noreply.github.com>
2022-02-12 23:50:38 -0800

    fix: limited the search queries (#32)

--- src/components/QuerySelectorList/QuerySelectorList.tsx
+++ src/components/QuerySelectorList/QuerySelectorList.tsx
@@ -7,15 +7,16 @@ import { QueriesContext } from "src/contexts/queries/queries";
 
 const QuerySelectorList = () => {
   const { queries, addQuery } = useContext(QueriesContext);
+  const MAX_QUERIES = 5;
 
   return (
     <menu className="grid grid-flow-col auto-cols-max gap-2 items-center mt-5">
       {Array.from(queries.entries()).map(([key]) => (
         <QuerySelector key={key} name="Search Query" queryId={key} />
       ))}
-      <button className="text-[24px] dark:text-white" onClick={addQuery}>
+      {queries.size < MAX_QUERIES && <button className="text-[24px] dark:text-white" onClick={addQuery}>
         <Add />
-      </button>
+      </button>}
     </menu>
   );
 };
