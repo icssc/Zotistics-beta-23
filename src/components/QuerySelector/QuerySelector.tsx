3f7bd1ca89324f73a09a10ebf4fea8aee49934e1
Alexander Liu <hey@alexanderliu.com>
2022-05-27 06:50:36 +0000

    style: reformat code

--- src/components/QuerySelector/QuerySelector.tsx
+++ src/components/QuerySelector/QuerySelector.tsx
@@ -3,50 +3,52 @@ import { Loader, X } from "react-feather";
 import { QueriesContext } from "src/contexts/queries/queries";
 
 interface QueryProps {
-    name: string;
-    queryId: any;
-    color: string | null;
+  name: string;
+  queryId: any;
+  color: string | null;
 }
 
-const Query = ({name, queryId, color}: QueryProps) => {
-    const {states, removeQuery, selectedQuery, setSelectedQuery} = useContext(QueriesContext);
+const Query = ({ name, queryId, color }: QueryProps) => {
+  const { states, removeQuery, selectedQuery, setSelectedQuery } =
+    useContext(QueriesContext);
 
   return (
     <div className="grid min-w-[20%]">
-        <div
-            className={`flex flex-row justify-between gap-1 items-center cursor-pointer py-2 pr-1 pl-2 text-sm rounded-lg dark:text-slate-100
-                        bg-[#f2f2f2] hover:bg-[#e7e7e7] dark:bg-[#171717] dark:hover:bg-[#363636]
-                        ${selectedQuery === queryId && "bg-[#d4d4d4] dark:bg-[#303030]"}`}
-            onClick={() => {
-                setSelectedQuery(queryId);
-            }}
-        >
-            {states.get(queryId)?.loading && (
-                <span className="animate-spin">
-                    <Loader size={16} strokeWidth={1.5}/>
-                </span>
-            )}
-            <div className="flex justify-center w-full">
-                <span>{name}</span>
-            </div>
-            <div className="flex">
-                <button
-                    onClick={() => {
-                        removeQuery(queryId);
-                    }}
-                >
-                  <span className="text-base">
-                    <X size={16} strokeWidth={1.5}/>
-                  </span>
-                </button>
-            </div>
-        </div>
-        {color && (
-            <span
-                className={`h-[2px] w-[97%] rounded-[99px] mt-1 mx-auto mb-0 ${selectedQuery === queryId && "h-[4px]"}`}
-                style={{ backgroundColor: color }}
-            />
+      <div
+        className={`flex flex-row justify-between gap-1 items-center cursor-pointer py-2 pr-1 pl-2 text-sm rounded-lg dark:text-slate-100 bg-[#f2f2f2] hover:bg-[#e7e7e7] dark:bg-[#171717] dark:hover:bg-[#363636]
+        ${selectedQuery === queryId && "bg-[#d4d4d4] dark:bg-[#303030]"}`}
+        onClick={() => {
+          setSelectedQuery(queryId);
+        }}
+      >
+        {states.get(queryId)?.loading && (
+          <span className="animate-spin">
+            <Loader size={16} strokeWidth={1.5} />
+          </span>
         )}
+        <div className="flex justify-center w-full">
+          <span>{name}</span>
+        </div>
+        <div className="flex">
+          <button
+            onClick={() => {
+              removeQuery(queryId);
+            }}
+          >
+            <span className="text-base">
+              <X size={16} strokeWidth={1.5} />
+            </span>
+          </button>
+        </div>
+      </div>
+      {color && (
+        <span
+          className={`h-[2px] w-[97%] rounded-[99px] mt-1 mx-auto mb-0 ${
+            selectedQuery === queryId && "h-[4px]"
+          }`}
+          style={{ backgroundColor: color }}
+        />
+      )}
     </div>
   );
 };
