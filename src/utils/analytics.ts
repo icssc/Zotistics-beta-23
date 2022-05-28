d9496e968d61ee110d43a4879f004680a612452b
Maybe14 <jchowdhury36@gmail.com>
2022-05-28 15:05:43 -0700

    adds definitions

---
+++ src/utils/analytics.ts
@@ -0,0 +1,32 @@
+import ReactGA4 from 'react-ga4';
+
+const analytics = {
+    nav: {
+        category: 'Navbar',
+        actions: {
+            OLD_ZOTISTICS_LINK: 'Old Zotistics Link',
+            HOME_LOGO: 'Click Home Logo',
+            FAQ: 'Click FAQ',
+            FEEDBACK: 'Click Feedback',
+            THEME: 'Change Theme'
+        }
+    },
+    search: {
+        category: "Search Query",
+        actions: {
+            INSTRUCTOR: 'Instructor',
+            QUARTER: 'Quarter',
+            YEAR: 'Year',
+            DEPARTMENT: 'Department',
+            COURSE_NUMBER: 'Course Number',
+            COURSE_CODE: 'Course Code'
+        }
+    },
+    data: {
+        category: "Data",
+        actions: {
+            COMPARE_BUTTON: 'Clicks Compare'
+        }
+    }
+
+}
